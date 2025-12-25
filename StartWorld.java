f2711c728270b992a276bdb7dd7d83f82c4d5317
idekakdj <paul.chapman2201@gmail.com>
2025-12-25 01:01:10 -0500

    d

---
+++ StartWorld.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Starting world screen leads to intro.
+ * 
+ * @author Paul 
+ */
+
+public class StartWorld extends World
+{
+    GreenfootImage startworld = new GreenfootImage ("startworld.jpg");
+    /**
+     * Constructor for objects of class StartWorld.
+     * 
+     */
+    public StartWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        setBackground(startworld);
+        //Button start = new Button ();
+        
+    }
+}
